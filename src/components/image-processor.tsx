b91888455a668e0b6a83e621f5986114a9300682
rob <heidlessemail16@gmail.com>
2025-10-25 16:45:24 +0000

    In `ImageProcessor.tsx`, add an HTML file input (`<input type='file'>`)

--- src/components/image-processor.tsx
+++ src/components/image-processor.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export function ImageProcessor() {
   return (
@@ -8,8 +10,9 @@ export function ImageProcessor() {
       <CardHeader>
         <CardTitle>Image Processor</CardTitle>
       </CardHeader>
-      <CardContent>
-        <p>This is where the image processing functionality will go.</p>
+      <CardContent className="space-y-4">
+        <Input type="file" accept="image/*" />
+        <Button disabled>Upload Images</Button>
       </CardContent>
     </Card>
   );
